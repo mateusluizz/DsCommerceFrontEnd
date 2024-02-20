43b9e33b13971f650a6ad1150906b9314800fad8
mateusluizz <mateusluizz@hotmail.com>
2024-02-19 20:05:26 -0400

    First Render

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,55 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import computerImg from "./assets/images/computer.png";
+import cartImg from "./assets/images/cart.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <header className="dsc-header-client">
+        <nav className="dsc-container">
+          <h1>DSCommerce</h1>
+          <div className="dsc-navbar-right">
+            <div className="dsc-menu-items-container">
+              <div className="dsc-menu-item">
+                <img src={cartImg} alt="Carrinho de compras" />
+              </div>
+            </div>
+            <a href="#">Entrar</a>
+          </div>
+        </nav>
+      </header>
+      <main>
+        <section id="product-details-section" className="dsc-container">
+          <div className="dsc-card dsc-mb20">
+            <div className="dsc-product-details-top dsc-line-bottom">
+              <img src={computerImg} alt="Computador" />
+            </div>
+            <div className="dsc-product-details-bottom">
+              <h3>R$ 5000,00</h3>
+              <h4>Computador Gamer XT</h4>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </p>
+              <div className="dsc-category-container">
+                <div className="dsc-category">Eletrônicos</div>
+                <div className="dsc-category">Computadores</div>
+              </div>
+            </div>
+          </div>
+          <div className="dsc-btn-page-container">
+            <div className="dsc-btn dsc-btn-blue">Comprar</div>
+            <div className="dsc-btn dsc-btn-white">Início</div>
+          </div>
+        </section>
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
